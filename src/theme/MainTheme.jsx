95854c0df723fc9965bddc4bc5455c1d8e61fb4d
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2023-03-03 11:35:14 +0100

    adding dark theme

--- src/theme/MainTheme.jsx
+++ src/theme/MainTheme.jsx
@@ -1,6 +1,6 @@
 import { createTheme } from "@mui/material/styles";
 
-export const theme = createTheme({
+export const LightTheme = createTheme({
  palette: {
   type: "light",
   primary: {
@@ -53,3 +53,58 @@ export const theme = createTheme({
   },
  },
 });
+
+
+export const darkTheme = createTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+          main: '#3f51b5',
+        },
+        secondary: {
+          main: '#f50057',
+        },
+        error: {
+          main: '#f44336',
+        },
+        success: {
+          main: '#4caf50',
+        },
+        warning: {
+          main: '#ff9800',
+        },
+        info: {
+          main: '#2196f3',
+        },
+      },
+      typography: {
+        h1: {
+          fontFamily: 'Tilt Neon',
+        },
+        h2: {
+          fontFamily: 'Tilt Neon',
+        },
+        fontFamily: 'Tilt Neon',
+        fontSize: 16,
+      },
+      shape: {
+        borderRadius: 4,
+      },
+      props: {
+        MuiTooltip: {
+          arrow: true,
+        },
+        MuiAppBar: {
+          color: 'default',
+        },
+        MuiList: {
+          dense: true,
+        },
+        MuiMenuItem: {
+          dense: true,
+        },
+        MuiTable: {
+          size: 'small',
+        },
+      },
+  });
